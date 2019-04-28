d04707dab26e3219d2168fc0676b0bc583ce64d8
jbrunton4 <39703189+jbrunton4@users.noreply.github.com>
2019-04-28 10:10:23 +0100

    test

--- CSS_Effects/Social Media Buttons/assets/script.js
+++
@@ -1,9 +0,0 @@
-function facebook() {
-    window.location.href = 'https://www.facebook.com';
-}
-function appstore() {
-    window.location.href = 'https://www.itunes.com';
-}
-function instagram() {
-    window.location.href = 'https://www.instagram.com';
-}
--- IP/assets/script.js
+++
@@ -1,9 +0,0 @@
-function findIp() {
-  var first = Math.floor((Math.random() * 1000) + 1);
-  var second = Math.floor((Math.random() * 1000) + 1);
-  var third = Math.floor((Math.random() * 100) + 1);
-  var fourth = Math.floor((Math.random() * 10) + 1);
-  alert('IP Addr. : ' + first.toString() + '.' + second.toString() + '.' + third.toString() + '.' + fourth.toString());
-  var ip = first.toString() + '.' + second.toString() + '.' + third.toString() + '.' + fourth.toString()
-  window.location.href='https://iplocation.com/';
-}
