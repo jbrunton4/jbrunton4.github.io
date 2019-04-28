8a8bb3f9812acf3a2e8329d0e7bc50769f126e7e
jbrunton4 <39703189+jbrunton4@users.noreply.github.com>
2019-04-28 10:11:38 +0100

    Initial Comitt

---
+++ CSS_Effects/Social Media Buttons/assets/script.js
@@ -0,0 +1,9 @@
+function facebook() {
+    window.location.href = 'https://www.facebook.com';
+}
+function appstore() {
+    window.location.href = 'https://www.itunes.com';
+}
+function instagram() {
+    window.location.href = 'https://www.instagram.com';
+}
---
+++ GCSE_Site/Computing/assets/script.js
@@ -0,0 +1,48 @@
+function getVote() {
+    const vote = document.getElementById('vote').value;
+    if (vote == 'English') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: English&body = 'I would like to vote for English to be the next page.");
+    } else if (vote == 'Maths') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Maths&body = 'I would like to vote for Maths to be the next page.");
+    } else if (vote == 'Physics') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Physics&body = 'I would like to vote for Physics to be the next page.");
+    } else if (vote == 'Chemistry') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Chemistry&body = 'I would like to vote for Chemistry to be the next page.");
+    } else if (vote == 'Biology') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Biology&body = 'I would like to vote for Biology to be the next page.");
+    } else if (vote == 'Maths for Science') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Maths for Science&body = 'I would like to vote for Maths for Science to be the next page.");
+    } else if (vote == 'History') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: History&body = 'I would like to vote for History to be the next page.");
+    } else if (vote == 'Geography') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Geography&body = 'I would like to vote for Geography to be the next page.");
+    } else if (vote == 'Media') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Media&body = 'I would like to vote for Media to be the next page.");
+    } else if (vote == 'Business') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Business&body = 'I would like to vote for Business to be the next page.");
+    } else if (vote == 'Core PE') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Core PE&body = 'I would like to vote for Core PE to be the next page.");
+    } else if (vote == 'GCSE PE') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: GCSE PE&body = 'I would like to vote for GCSE PE to be the next page.");
+    } else if (vote == 'French') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: French&body = 'I would like to vote for French to be the next page.");
+    } else if (vote == 'Psychology') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Psychology&body = 'I would like to vote for Psychology to be the next page.");
+    } else if (vote == 'RE') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: RE&body = 'I would like to vote for RE to be the next page.");
+    } else if (vote == 'Food') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Food&body = 'I would like to vote for Food to be the next page.");
+    } else if (vote == 'Music') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Music&body = 'I would like to vote for Music to be the next page.");
+    } else if (vote == 'Basic Skills') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Basic Skills&body = 'I would like to vote for Basic Skills to be the next page.");
+    } else if (vote == 'Art') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Art&body = 'I would like to vote for Art to be the next page.");
+    } else if (vote == 'DT') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: DT&body = 'I would like to vote for DT to be the next page.");
+    } else if (vote == 'Drama') {
+        window.open("mailto:16-016@wetherbyhigh.co.uk?subject=Website Vote: Drama&body = 'I would like to vote for Drama to be the next page.");
+    } else {
+        alert('Sorry, there was a problem.');
+    }
+}
